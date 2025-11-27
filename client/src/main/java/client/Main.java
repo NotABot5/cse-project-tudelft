c1a06f7fa5c27a0a04381e4d9846a2ccdb95c2cb
Andrei Bălăiță <a.balaita@student.tudelft.nl>
2025-11-27 18:07:37 +0100

    Update the main file.

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -1,7 +1,16 @@
 package client;
 
+import client.utils.ServerUtils;
+import commons.Recipe;
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main extends Application {
 
@@ -11,6 +20,20 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
+        var recipeListView = new ListView<Recipe>();
+
+        List<Recipe> fakeRecipes = new ArrayList<>();
+        fakeRecipes.add(new Recipe("Spaghetti Carbonara", "en", new ArrayList<>()));
+        fakeRecipes.add(new Recipe("Chicken Noodle Soup", "en", new ArrayList<>()));
+
+        recipeListView.setItems(FXCollections.observableArrayList(ServerUtils.getRecipes()));
+
+
+        var mainLayout = new VBox();
+        mainLayout.getChildren().addAll(recipeListView);
+
+        var scene = new Scene(mainLayout, 600, 400);
+        primaryStage.setScene(scene);
         primaryStage.setTitle("Food Backlog");
         primaryStage.show();
     }
