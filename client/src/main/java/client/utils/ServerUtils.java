0df2848f5820461bbbd62a6d2b44d0308d6359cf
Justin Romeijn <j.l.romeijn@student.tudelft.nl>
2025-11-30 23:19:13 +0100

    Client to server updating adding and deleting

--- client/src/main/java/client/utils/ServerUtils.java
+++ client/src/main/java/client/utils/ServerUtils.java
@@ -2,6 +2,8 @@ package client.utils;
 
 import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
 
+import commons.Ingredient;
+import jakarta.ws.rs.client.Entity;
 import org.glassfish.jersey.client.ClientConfig;
 
 import commons.Recipe;
@@ -30,4 +32,37 @@ public class ServerUtils {
                     .request(APPLICATION_JSON).get(new GenericType<List<String>>() {});
         }
     }
+
+        public Recipe addRecipe(Recipe recipe) {
+            try (var client = ClientBuilder.newClient(new ClientConfig())) {
+                return client.target(SERVER).path("api/recipes")
+                        .request(APPLICATION_JSON)
+                        .post(Entity.entity(recipe, APPLICATION_JSON), Recipe.class);
+            }
+        }
+
+        public void deleteRecipe(long id) {
+            try (var client = ClientBuilder.newClient(new ClientConfig())) {
+                client.target(SERVER).path("api/recipes/" + id)
+                        .request(APPLICATION_JSON)
+                        .delete();
+            }
+        }
+
+        public Ingredient addIngredient(Ingredient ingredient) {
+            try (var client = ClientBuilder.newClient(new ClientConfig())) {
+                return client.target(SERVER).path("api/ingredients")
+                        .request(APPLICATION_JSON)
+                        .post(Entity.entity(ingredient, APPLICATION_JSON), Ingredient.class);
+            }
+        }
+
+        public void deleteIngredient(long id) {
+            try (var client = ClientBuilder.newClient(new ClientConfig())) {
+            client.target(SERVER).path("api/ingredients/" + id)
+                    .request(APPLICATION_JSON)
+                    .delete();
+            }
+        }
+
 }
