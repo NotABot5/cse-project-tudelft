e214696af6f75a691dbc27d23bc6d2d2644f0623
Andrei Ianuc <a.ianuc@student.tudelft.nl>
2025-12-04 16:17:58 +0100

    Add Download an Refresh button UI, comment on AI usage and small indentation improvements

--- client/src/main/java/client/Main.java
+++ client/src/main/java/client/Main.java
@@ -5,8 +5,10 @@ import javafx.application.Application;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextField;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -16,6 +18,9 @@ public class Main extends Application {
     @FXML protected TextField fieldCarbs;
     @FXML protected TextField fieldProtein;
     @FXML protected ChoiceBox<Ingredient> ingredientChoiceBox;
+
+    @FXML protected Button refreshbutton;
+    @FXML protected Button downloadbutton;
 
 
 
@@ -116,5 +121,28 @@ public class Main extends Application {
         fieldFat.clear();
         fieldCarbs.clear();
         fieldProtein.clear();
+    }
+
+    /**
+     * Refetches all information
+     */
+    @FXML
+    public void onRefresh(){
+        refreshbutton.setDisable(true);
+        refreshbutton.setText("Refresh");
+
+        refreshbutton.setDisable(false);
+        System.out.println("Refreshed:");
+
+    }
+
+    /**
+     * Allow the user to download a recipe
+     */
+    @FXML
+    public void onDownload(){
+        downloadbutton.setDisable(true);
+        downloadbutton.setDisable(false);
+        System.out.println("Downloaded:");
     }
 }
