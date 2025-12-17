ad5fdc299b97463f0a2ed04cc8a4b27bb98dcc15
Jayden Wong <j.y.p.wong@student.tudelft.nl>
2025-12-17 02:46:08 +0100

    Improved converting toString

--- commons/src/main/java/commons/IngredientUsage.java
+++ commons/src/main/java/commons/IngredientUsage.java
@@ -5,6 +5,8 @@ import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
 import org.apache.commons.lang3.builder.ToStringBuilder;
 
+import java.math.BigDecimal;
+
 import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
 
 @Entity
@@ -93,9 +95,9 @@ public class IngredientUsage {
      * Determines the amount of ingredient needed, when converting to its basic unit
      * @return the amount of ingredient needed after conversion
      */
-    public int convertedAmount() {
+    public double convertedAmount() {
         if (getUnit().getFormal()) {
-            return (int) (getAmount() * getUnit().getConversionFactorToBase());
+            return(getAmount() * getUnit().getConversionFactorToBase());
         }
         else{
             return(0);
@@ -104,13 +106,13 @@ public class IngredientUsage {
 
     /**
      * Shows amount of ingredient needed, when converting to its basic unit in a readable format
-     * @return
+     * @return String containing amount of ingredient in standard unit in readable format
      */
-    public String convertedAmountToString() {
-        int convertedAmount = convertedAmount();
+    public String convertedToString() {
+        double convertedAmount = convertedAmount();
         StringBuilder res = new StringBuilder();
         if (getAmount() != 0 && getUnit().getType() != UnitType.COUNT) {
-            res.append(convertedAmount);
+            res.append(BigDecimal.valueOf(convertedAmount).stripTrailingZeros().toPlainString());
             res.append(" ");
 
             if (getUnit().getType() == UnitType.MASS) {
@@ -124,11 +126,69 @@ public class IngredientUsage {
         }
         if (getUnit().getType() != UnitType.COUNT) {res.append(" of ");}
         else {
-            res.append(getAmount());
+            res.append(BigDecimal.valueOf(getAmount()).stripTrailingZeros().toPlainString());
             res.append(" ");
         }
         res.append(getIngredient().getName());
         return(res.toString());
     }
 
+    /**
+     * Produces string of amount ingredient when converted to a specified unit, given that it is compatible
+     * The compatibility conditions are as follows:
+     * Both ingredient units are of the same type (MASS, VOLUME, COUNT)
+     * Both ingredient units are formal
+     * NOTE: targetUnit may not have conversion factor of 0
+     * NOTE: Currently, trailing zeros are removed, but converted amounts are not rounded
+     * NOTE: For simplicity, Results may miss plural forms if not specified
+     * @param targetUnit Unit in which amount of ingredient needed should be expressed
+     * @return String converted to specified unit in readable format
+     */
+    public String convertedToString(IngredientUnit targetUnit) {
+        StringBuilder res = new StringBuilder();
+        //Check whether unit types are compatible, otherwise ingredient amount cannot be converted
+        //If so, mention this incompatibility through to end result
+        //Additionally, incompatibility occurs if one or both of the units are informal
+        IngredientUnit thisUnit = getUnit();
+        if (thisUnit.getType() != targetUnit.getType()
+                || !thisUnit.getFormal()
+                || !targetUnit.getFormal()) {
+            res.append("Unknown quantity of ");
+        }
+        //Otherwise, convert ingredient amount
+        else {
+            double baseUnitAmount = convertedAmount();
+            double targetAmount = baseUnitAmount / targetUnit.getConversionFactorToBase();
+            //Check whether amount of "whole" ingredients is measured and targetUnit has no name
+            //Example: 5 apples instead of "5 of apple"
+            if (targetUnit.getName().isEmpty() && targetUnit.getType() == UnitType.COUNT) {
+                return(convertedToStringShortFormat(targetUnit, res, targetAmount));
+            }
+            //After conversion, remove trailing zeros if present
+            else {
+                res.append(BigDecimal.valueOf(targetAmount).stripTrailingZeros().toPlainString());
+                res.append(" ");
+                res.append(targetUnit.getName());
+                res.append(" of ");
+            }
+        }
+        res.append(getIngredient().getName());
+        return (res.toString());
+    }
+
+    /** Helper method for special case of target unit being "empty count" unit
+     *  Example: "5 broccoli" instead of "5 of broccoli"
+     * @param targetUnit Unit in which amount of ingredient needed should be expressed
+     * @param res Empty StringBuilder
+     * @param amount Converted ingredient amount (may contain trailing zeros)
+     * @return String converted to specified unit in readable format
+     */
+
+    public String convertedToStringShortFormat(IngredientUnit targetUnit, StringBuilder res, double amount) {
+        //Strip trailing zeros if present from conversion
+        res.append(BigDecimal.valueOf(amount).stripTrailingZeros().toPlainString());
+        res.append(" ");
+        res.append(getIngredient().getName());
+        return(res.toString());
+    }
 }
